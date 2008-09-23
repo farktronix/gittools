e6e77cc1fb55bb34620c6bdbb5984e86a3182099
Dan Preston <goober2@mac.com>
2008-09-22 23:45:28 -0700

    Added two scripts that may come in handy when using xcode and git together.

---
+++ xcode_scripts/xcode_git_bbdiff.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+#
+# This script is intended to be used from the xcode script menu, rather than from the terminal. Open the script editor in
+# xcode and add the script file with the settings below.
+
+# This script will open the frontmost file using git-bbdiff. If there are no differences, it will beep twice.
+#
+#	Suggested settings:
+#		Input:		No Input
+#		Directory:	Selection
+#		Output:		Discard Output
+#		Errors:		Display in Alert
+#
+
+import sys
+import commands
+import AppKit
+import time
+
+filename = "%%%{PBXFilePath}%%%"
+
+command = "/usr/local/bin/git bbdiff " + filename
+
+print( command )
+status, output = commands.getstatusoutput( command )
+print( output )
+
+if output.count( "has no differences." ) > 0:
+	# We need to sleep for a bit after each beep to allow
+	# the sound to play or else it sounds jacked up.
+	AppKit.NSBeep()
+	time.sleep(0.17)
+	AppKit.NSBeep()
+	time.sleep(0.17)
---
+++ xcode_scripts/xcode_git_log.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+#
+# This script is intended to be used from the xcode script menu, rather than from the terminal. Open the script editor in
+# xcode and add the script file with the settings below.
+#
+# When run from the script menu, this script will display the git log for the frontmost file.
+#
+#	Suggested settings:
+#		Input:		No Input
+#		Directory:	Selection
+#		Output:		Open in New Document
+#		Errors:		Merge with Script Output
+#
+
+import sys
+import commands
+import os
+
+filename = "%%%{PBXFilePath}%%%"
+
+command = "/usr/local/bin/git log " + filename
+
+print( "+----------------------------------------------------------------------------------------------------------------------------" )
+print( "| git log \"" + os.path.basename( filename ) + "\"" )
+print( "+----------------------------------------------------------------------------------------------------------------------------\n\n" )
+
+status, output = commands.getstatusoutput( command )
+print( output )
