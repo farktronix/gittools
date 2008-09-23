f9aa345865cedd2c6b5ec8fdce8d489720ffc9fa
Dan Preston <goober2@mac.com>
2008-09-23 02:00:14 -0700

    If there are no differences from HEAD, bbdiff against the last version of the file in the repository that had a change.

--- xcode_scripts/xcode_git_bbdiff.py
+++ xcode_scripts/xcode_git_bbdiff.py
@@ -18,18 +18,43 @@ import commands
 import AppKit
 import time
 
+#----------------------------------------------------------------------------------------------------------------------------
+#	beepError
+#----------------------------------------------------------------------------------------------------------------------------
+
+def beepError( inTimes = 2 ):
+	i = 0
+	while i < inTimes:
+		AppKit.NSBeep()
+		time.sleep(0.17)
+		i = i + 1
+
+#----------------------------------------------------------------------------------------------------------------------------
+#	main script
+#----------------------------------------------------------------------------------------------------------------------------
+
 filename = "%%%{PBXFilePath}%%%"
 
 command = "/usr/local/bin/git bbdiff " + filename
 
 print( command )
 status, output = commands.getstatusoutput( command )
+if status != 0:
+	beepError()
 print( output )
 
 if output.count( "has no differences." ) > 0:
-	# We need to sleep for a bit after each beep to allow
-	# the sound to play or else it sounds jacked up.
-	AppKit.NSBeep()
-	time.sleep(0.17)
-	AppKit.NSBeep()
-	time.sleep(0.17)
+	versions = []
+	command = "/usr/local/bin/git log " + filename
+	status, output = commands.getstatusoutput( command )
+	if status != 0:
+		beepError()
+	
+	for line in output.splitlines():
+		if line.startswith( "commit " ):
+			versions.append( line.replace( "commit ", "" )[0:8] )
+			
+	command = "/usr/local/bin/git bbdiff -r %s %s" % (versions[1], filename)
+	status, output = commands.getstatusoutput( command )
+	if status != 0:
+		beepError()
